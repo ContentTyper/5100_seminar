5ebabdd26cb6454963abbdbb3818d43793b52923
ContentTyper <110904863+ContentTyper@users.noreply.github.com>
2021-06-05 16:26:14 +0000

    Gems updated

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,11 @@ use std::io::{ self, Read };
 use std::path::{ Path, PathBuf };
 use argh::FromArgs;
 use anyhow::Context;
+use bstr::ByteSlice;
+use encoding_rs::Encoding;
+use rayon::prelude::*;
+use memmap2::MmapOptions;
+use flate2::bufread::DeflateDecoder;
+use zstd::stream::read::Decoder as ZstdDecoder;
+use zip_parser::{ compress, system, ZipArchive, CentralFileHeader };
+use util::{

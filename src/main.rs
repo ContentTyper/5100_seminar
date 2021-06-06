37b4094fab55437889e045056036535f462aa4cc
ContentTyper <110904863+ContentTyper@users.noreply.github.com>
2021-06-06 10:04:35 +0000

    Updating composer

--- src/main.rs
+++ src/main.rs
@@ -14,3 +14,20 @@ use flate2::bufread::DeflateDecoder;
 use zstd::stream::read::Decoder as ZstdDecoder;
 use zip_parser::{ compress, system, ZipArchive, CentralFileHeader };
 use util::{
+    Decoder, Crc32Checker, FilenameEncoding,
+    dos2time, path_join, path_open, sanitize_setuid
+};
+
+
+/// unzrip - extract compressed files in a ZIP archive
+#[derive(FromArgs)]
+struct Options {
+    /// path of the ZIP archive(s).
+    #[argh(positional)]
+    file: Vec<PathBuf>,
+
+    /// an optional directory to which to extract files.
+    #[argh(option, short = 'd')]
+    exdir: Option<PathBuf>,
+
+    /// specify character set used to decode filename,

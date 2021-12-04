7c2cc942d867e691193e12fd1dde32cc6c435b1d
ContentTyper <110904863+ContentTyper@users.noreply.github.com>
2021-12-04 18:12:46 +0000

    x86, efi: Fix processor-specific memcpy() build error

--- src/util.rs
+++ src/util.rs
@@ -46,4 +46,5 @@ impl<R: io::Read> io::Read for Crc32Checker<R> {
         if n == 0 {
             let crc = self.hasher.clone().finalize();
             if crc != self.expect {
-                let msg = format!("crc32 check failed. expect: {}, got: 
+                let msg = format!("crc32 check failed. expect: {}, got: {}",
+        

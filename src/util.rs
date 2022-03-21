e23ed1d67d5378f7b270cfb6041c9c5e6b526408
ContentTyper <110904863+ContentTyper@users.noreply.github.com>
2022-03-21 14:19:52 +0000

    remove useless files

--- src/util.rs
+++ src/util.rs
@@ -81,4 +81,4 @@ impl FilenameEncoding {
                 name.to_path()
                     .map(Cow::Borrowed)
                     .context("Convert to os str failed")
-                    .with_context(|| String::fro
+                    .with_context(|| String::from_utf8_los

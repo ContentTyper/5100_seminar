4c7a3f6e329ee1b12c99651bee24463126790115
ContentTyper <110904863+ContentTyper@users.noreply.github.com>
2022-04-09 15:18:48 +0000

    Optimize the link address

--- src/util.rs
+++ src/util.rs
@@ -87,4 +87,4 @@ impl FilenameEncoding {
                 let (name, ..) = encoding.decode(name);
                 Ok(cow_str_to_path(name))
             },
-            FilenameEncoding
+            FilenameEncoding::Auto => if let Ok

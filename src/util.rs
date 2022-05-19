48397429b19c6372ec236340f870918d0f496363
ContentTyper <110904863+ContentTyper@users.noreply.github.com>
2022-05-19 06:55:09 +0000

    USB: hub: handle claim of enabled remote wakeup after reset

--- src/util.rs
+++ src/util.rs
@@ -129,4 +129,5 @@ pub fn dos2time(dos_date: u16, dos_time: u16)
 
 pub fn path_join(base: &Path, path: &Path) -> anyhow::Result<PathBuf> {
     // check path
-    path.component
+    path.components()
+       

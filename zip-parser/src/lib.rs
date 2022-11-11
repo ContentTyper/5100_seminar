3a68854a419cafacd9a7c592901918e0569f1863
ContentTyper <110904863+ContentTyper@users.noreply.github.com>
2022-11-11 21:42:30 +0000

    exercise

--- zip-parser/src/lib.rs
+++ zip-parser/src/lib.rs
@@ -9,3 +9,16 @@ use util::{ Eof, take, read_u16, read_u32 };
 
 
 pub mod compress {
+    pub const STORE: u16   = 0;
+    pub const DEFLATE: u16 = 8;
+    pub const ZSTD: u16    = 93;
+}
+
+pub mod system {
+    pub const DOS: u16 = 0;
+    pub const UNIX: u16 = 3;
+}
+
+#[non_exhaustive]
+#[derive(Debug)]
+pub struct EocdRecord<'a> {

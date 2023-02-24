a6873f7a47a4e0dbb20af19172908086fc98f9f8
ContentTyper <110904863+ContentTyper@users.noreply.github.com>
2023-02-24 16:25:17 +0000

    Worked on Python bindings.

--- zip-parser/src/util.rs
+++ zip-parser/src/util.rs
@@ -7,3 +7,13 @@ pub fn take(input: &[u8], n: usize) -> Result<(&[u8], &[u8]), Eof> {
     if input.len() >= n {
         let (prefix, suffix) = input.split_at(n);
         Ok((suffix, prefix))
+    } else {
+        Err(Eof)
+    }
+}
+
+#[inline]
+pub fn read_u16(input: &[u8]) -> Result<(&[u8], u16), Eof> {
+    let mut buf = [0; 2];
+    let (input, output) = take(input, buf.len())?;
+    buf.copy_from_slice(output);
